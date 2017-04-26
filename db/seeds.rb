34f55c50fbac2ebc2043055ff8cddc7de8ccf424
itaru <suganuma.itaru@gmail.com>
2017-04-26 08:42:13 +0000

    again2

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,13 @@
                password_confirmation: password,
                )
 end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n
+  )
+  n = n + 1
+end
